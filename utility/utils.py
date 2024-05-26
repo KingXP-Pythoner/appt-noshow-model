39b51405696f0e1c89e70a817b018efc298f5706
KINGXP-PC <kingsleykelechi008@gmail.com>
2024-05-26 07:37:31 +0100

    add abstractions

---
+++ utility/utils.py
@@ -0,0 +1,342 @@
+# Create abstraction functions
+    # import the required libraries
+import pandas as pd
+import numpy as np
+from typing import Any, Dict, List, Optional, Tuple, Union
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
+# import Random undersampler from imblearn
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.preprocessing import MinMaxScaler
+
+    
+def split_df(df:pd.DataFrame, target_col_label:str) -> tuple:
+    """
+    Split the dataset into features and target variables.
+    
+    Args:
+        df (pd.DataFrame): The dataset to be split.
+    
+    Returns:
+    X (pd.DataFrame): The features.
+    y (pd.Series): The target variable.
+    """
+    X = df.drop(columns=[target_col_label], axis=1)
+    y = df[target_col_label]
+    return X, y
+
+def train_val_test_split(X:pd.DataFrame, selected_cols:List[str], lvl1_test_size:float, lvl2_test_size:float, target_col_label:str, random_state:int=42 ) -> tuple:
+    """
+    Split the dataset into training and testing sets.
+    
+    Args:
+        X (pd.DataFrame): The features.
+        y (pd.Series): The target variable.
+        test_size (float): The proportion of the dataset to include in the test split.
+        random_state (int): Controls the shuffling applied to the data before applying the split.
+    
+    Returns:
+    X_train (pd.DataFrame): The training features.
+    X_val (pd.DataFrame): The validation features.
+    X_test (pd.DataFrame): The testing features.
+    y_train (pd.Series): The training target variable.
+    y_val (pd.Series): The validation target variable.
+    y_test (pd.Series): The testing target variable.
+    
+    """
+    from sklearn.model_selection import train_test_split
+    # Initial stratified split into training and temp set (70% training, 30% temp)
+    train_set, temp_set = train_test_split(X, test_size=lvl1_test_size, random_state=42)
+
+    # Stratified split of the temp set into validation and test sets (each 15% of the total data)
+    val_set, test_set = train_test_split(temp_set, test_size=lvl2_test_size, random_state=42)
+    
+    return train_set[selected_cols], val_set[selected_cols], test_set[selected_cols], train_set[target_col_label], val_set[target_col_label], test_set[target_col_label]
+
+def under_sample(X_train:pd.DataFrame, y_train:pd.Series, random_state:int=42) -> tuple:
+    """
+    Undersample the training data to balance the classes.
+    
+    Args:
+        X_train (pd.DataFrame): The training features.
+        y_train (pd.Series): The training target variable.
+        random_state (int): Controls the shuffling applied to the data before applying the split.
+    
+    Returns:
+    X_train_resampled (pd.DataFrame): The resampled training features.
+    y_train_resampled (pd.Series): The resampled training target variable.
+    """
+    # Instantiate the RandomUnderSampler
+    rus = RandomUnderSampler(random_state=random_state)
+    # Resample the training data
+    X_train_resampled, y_train_resampled = rus.fit_resample(X_train, y_train)
+    return X_train_resampled, y_train_resampled
+
+def scale_features(X_train:pd.DataFrame, X_val:pd.DataFrame, X_test:pd.DataFrame) -> tuple:
+    """
+    Scale the features using MinMaxScaler.
+    
+    Args:
+        X_train (pd.DataFrame): The training features.
+        X_val (pd.DataFrame): The validation features.
+        X_test (pd.DataFrame): The testing features.
+    
+    Returns:
+    X_train_scaled (pd.DataFrame): The scaled training features.
+    X_val_scaled (pd.DataFrame): The scaled validation features.
+    X_test_scaled (pd.DataFrame): The scaled testing features.
+    """
+    # Instantiate the MinMaxScaler
+    scaler = MinMaxScaler()
+    # Fit the scaler on the training data
+    scaler.fit(X_train)
+    # Transform the training, validation, and testing data
+    X_train_scaled = scaler.transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_val_scaled, X_test_scaled
+
+def apply_frequency_encoding(df:pd.DataFrame, no_fmap_columns_dict:Optional[Dict[str, str]]=None, fmap: Optional[Dict[str, float]]=None, fmap_column_dict: Optional[Dict[str,str]]=None) -> Tuple[pd.DataFrame, Dict[Any , Union[int, float]]]:
+    """
+    Apply frequency encoding to the specified columns.
+    
+    Args:
+        df (pd.DataFrame): The dataset to be transformed.
+        no_fmap_columns_dict (dict): A dictionary where the keys are the columns to be transformed and the values are the new column names.
+        fmap (dict) (optional): An optional dictionary, if provided, the function will use the values in the dictionary as the frequency map instead of calculating the frequencies. if fmap is provided, fmap_column_dict must also be provided and it can only contain one key-value pair. no_fmap_columns_dict will be ignored if fmap is provided.
+    Returns:
+    df (pd.DataFrame): The dataset with frequency encoding applied.
+    
+    """
+    freq_dict: Dict[Any, Union[int, float]] = {}
+    if fmap != None and fmap_column_dict != None:
+        # If fmap is provided, use the values in the dictionary as the frequency map
+        for old_label, new_label in fmap_column_dict.items():
+            df[new_label] = df[old_label].map(fmap).fillna(0)
+    else:
+        # Calculate the frequencies of the values in the specified columns
+        if no_fmap_columns_dict != None:
+            for old_label, new_label in no_fmap_columns_dict.items():
+                freq = df[old_label].value_counts(normalize=True)
+                df[new_label] = df[old_label].map(freq)
+                freq_dict = freq.to_dict()
+    return df, freq_dict
+
+class ModelRunner():
+    """
+    Run the model and return the accuracy score, confusion matrix, classification report, and ROC AUC score. This function also plots the ROC AUC curve.
+
+    Args:
+        model (Any): The classification model.
+        X_train (pd.DataFrame): The training features.
+        X_test (pd.DataFrame): The testing features.
+        y_train (pd.Series): The training target variable.
+        y_test (pd.Series): The testing target variable.
+        X_val (pd.DataFrame): The validation features.
+        y_val (pd.Series): The validation target variable.
+    
+    Returns:
+    val_y_pred (np.ndarray): The predicted target values for the validation set.
+    val_y_pred_proba (np.ndarray): The predicted probabilities for the validation set.
+    val_accuracy (float): The accuracy score for the validation set.
+    val_confusion_matrix (np.ndarray): The confusion matrix for the validation set.
+    val_classification_report (str | dict): The classification report for the validation set.
+    val_roc_auc_score (float): The ROC AUC score for the validation set.
+    val_fpr (np.ndarray): The false positive rate for the validation set.
+    val_tpr (np.ndarray): The true positive rate for the validation set.
+    val_thresholds (np.ndarray): The thresholds for the validation set.
+    test_y_pred (np.ndarray): The predicted target values for the testing set.
+    test_y_pred_proba (np.ndarray): The predicted probabilities for the testing set.
+    test_accuracy (float): The accuracy score for the testing set.
+    test_confusion_matrix (np.ndarray): The confusion matrix for the testing set.
+    test_classification_report (str | dict): The classification report for the testing set.
+    test_roc_auc_score (float): The ROC AUC score for the testing set.
+    test_fpr (np.ndarray): The false positive rate for the testing set.
+    test_tpr (np.ndarray): The true positive rate for the testing set.
+    test_thresholds (np.ndarray): The thresholds for the testing set.
+    """
+    model: Any
+    # pandas.DataFrame
+    X_train: pd.DataFrame
+    y_train: pd.Series
+    X_val: pd.DataFrame
+    y_val: pd.Series
+    X_test: pd.DataFrame
+    y_test: pd.Series
+    val_y_pred: np.ndarray
+    val_y_pred_proba: np.ndarray
+    val_accuracy: float
+    val_confusion_matrix: np.ndarray
+    val_classification_report: str | dict
+    val_roc_auc_score: float
+    val_fpr: np.ndarray
+    val_tpr: np.ndarray
+    val_thresholds = None
+    test_y_pred: np.ndarray
+    test_y_pred_proba: np.ndarray
+    test_accuracy: float
+    test_confusion_matrix: np.ndarray
+    test_classification_report: str | dict
+    test_roc_auc_score: float
+    test_fpr: np.ndarray
+    test_tpr: np.ndarray
+    test_thresholds = None
+    pick_results: List[str] | None
+    
+    def __init__(self, model, X_train, X_test, y_train, y_test, X_val, y_val, pick_results:List[str] | None):
+        self.model = model
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.X_val = X_val
+        self.y_val = y_val
+        self.pick_results = pick_results
+        
+    
+    def __run_model__(self):
+        # Fit the model on the training data
+        self.model.fit(self.X_train, self.y_train)
+        # Predict the target values
+        self.val_y_pred = self.model.predict(self.X_val)
+        self.test_y_pred = self.model.predict(self.X_test)
+        # Predict the probabilities
+        self.val_y_pred_proba = self.model.predict_proba(self.X_val)[:, 1]
+        self.test_y_pred_proba = self.model.predict_proba(self.X_test)[:, 1]
+        # Calculate the accuracy score
+        self.val_accuracy = self.model.score(self.X_val, self.y_val)
+        self.test_accuracy = self.model.score(self.X_test, self.y_test)
+        # Calculate the confusion matrix
+        self.val_confusion_matrix = confusion_matrix(self.y_val, self.val_y_pred)
+        self.test_confusion_matrix = confusion_matrix(self.y_test, self.test_y_pred)
+        # Calculate the classification report
+        
+        self.val_classification_report = classification_report(self.y_val, self.val_y_pred)
+        self.test_classification_report = classification_report(self.y_test, self.test_y_pred)
+        # Calculate the ROC AUC score
+        self.val_roc_auc_score = float(roc_auc_score(self.y_val, self.val_y_pred_proba))
+        self.test_roc_auc_score = float(roc_auc_score(self.y_test, self.test_y_pred_proba))
+        # Calculate the ROC curve
+        self.val_fpr, self.val_tpr, self.val_thresholds = roc_curve(self.y_val, self.val_y_pred_proba)
+        self.test_fpr, self.test_tpr, self.test_thresholds = roc_curve(self.y_test, self.test_y_pred_proba)
+    
+    def __plot_roc_curve__(self):
+        for data in [(self.val_fpr, self.val_tpr, self.val_roc_auc_score, 'Validation'), (self.test_fpr, self.test_tpr, self.test_roc_auc_score, 'Test')]:
+            fpr, tpr, roc_auc_score, label = data
+            plt.plot(fpr, tpr, label=f'{label} (AUC = {roc_auc_score:.2f})')
+        plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        
+        
+    
+    def __returns_in_dict__(self):
+        # returns the results in a dictionary
+        return {
+            'val_y_pred': self.val_y_pred,
+            'val_y_pred_proba': self.val_y_pred_proba,
+            'val_accuracy': self.val_accuracy,
+            'val_confusion_matrix': self.val_confusion_matrix,
+            'val_classification_report': self.val_classification_report,
+            'val_roc_auc_score': self.val_roc_auc_score,
+            'val_fpr': self.val_fpr,
+            'val_tpr': self.val_tpr,
+            'val_thresholds': self.val_thresholds,
+            'test_y_pred': self.test_y_pred,
+            'test_y_pred_proba': self.test_y_pred_proba,
+            'test_accuracy': self.test_accuracy,
+            'test_confusion_matrix': self.test_confusion_matrix,
+            'test_classification_report': self.test_classification_report,
+            'test_roc_auc_score': self.test_roc_auc_score,
+            'test_fpr': self.test_fpr,
+            'test_tpr': self.test_tpr,
+            'test_thresholds': self.test_thresholds
+        }
+        
+    def __pretty_print__(self):
+        # Pretty print the results
+        print(F'Validation Set\n')
+        print(f'Accuracy: {self.val_accuracy:.2f}')
+        # print validation confusion matrix as a table with labels of true positive, false positive, true negative, and false negative
+        print(f'Confusion Matrix:\n{pd.DataFrame(self.val_confusion_matrix, columns=["Predicted Negative", "Predicted Positive"], index=["Actual Negative", "Actual Positive"])}')
+        print(f'Classification Report:\n{self.val_classification_report}')
+        print(f'ROC AUC Score: {self.val_roc_auc_score:.2f}')
+        print('Test Set')
+        print(f'Accuracy: {self.test_accuracy:.2f}')
+        print(f'Confusion Matrix:\n{pd.DataFrame(self.test_confusion_matrix, columns=["Predicted Negative", "Predicted Positive"], index=["Actual Negative", "Actual Positive"])}')
+        print(f'Classification Report:\n{self.test_classification_report}')
+        print(f'ROC AUC Score: {self.test_roc_auc_score:.2f}')
+        plt.legend()
+        plt.show()
+        
+    def invoke(self):
+        self.__run_model__()
+        self.__plot_roc_curve__()
+        if self.pick_results != None:
+            # For each key in the pick_results list, fetch the corresponding value from the dictionary and return all in a tuple
+            return_list = []
+            for key in self.pick_results:
+                if key not in self.__returns_in_dict__().keys():
+                    raise KeyError(f"KeyError: '{key}' not found in the dictionary")
+                return_list.append(self.__returns_in_dict__()[key])
+            self.__pretty_print__()
+            return tuple(return_list)
+        
+
+
+# Create a pipeline that will split the dataset, undersample the training data, scale the features, and run the model
+def run_pipeline(df:pd.DataFrame, target_col_label:str, model:Any, selected_cols:List[str], lvl1_test_size:float, lvl2_test_size:float, pick_results:List[str] | None, random_state:int=42):
+    """
+    Run the pipeline that will split the dataset, undersample the training data, apply frequency encoding, scale the features, and run the model.
+    
+    Args:
+        df (pd.DataFrame): The dataset to be used.
+        target_col_label (str): The label of the target column.
+        model (Any): The classification model.
+        selected_cols (list): The columns to be used as features.
+        test_size (float): The proportion of the dataset to include in the test split.
+        random_state (int): Controls the shuffling applied to the data before applying the split.
+    
+    Returns:
+    val_y_pred (np.ndarray): The predicted target values for the validation set.
+    val_y_pred_proba (np.ndarray): The predicted probabilities for the validation set.
+    val_accuracy (float): The accuracy score for the validation set.
+    val_confusion_matrix (np.ndarray): The confusion matrix for the validation set.
+    val_classification_report (str | dict): The classification report for the validation set.
+    val_roc_auc_score (float): The ROC AUC score for the validation set.
+    val_fpr (np.ndarray): The false positive rate for the validation set.
+    val_tpr (np.ndarray): The true positive rate for the validation set.
+    val_thresholds (np.ndarray): The thresholds for the validation set.
+    test_y_pred (np.ndarray): The predicted target values for the testing set.
+    test_y_pred_proba (np.ndarray): The predicted probabilities for the testing set.
+    test_accuracy (float): The accuracy score for the testing set.
+    test_confusion_matrix (np.ndarray): The confusion matrix for the testing set.
+    test_classification_report (str | dict): The classification report for the testing set.
+    test_roc_auc_score (float): The ROC AUC score for the testing set.
+    test_fpr (np.ndarray): The false positive rate for the testing set.
+    test_tpr (np.ndarray): The true positive rate for the testing set.
+    test_thresholds (np.ndarray): The thresholds for the testing set.
+    """
+    # Split the dataset into features and target variables
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(df, selected_cols, lvl1_test_size, lvl2_test_size, target_col_label, random_state)
+    
+    # Encode the neighbourhood column using frequency encoding for the training set
+    X_train, train_fmap = apply_frequency_encoding(X_train, no_fmap_columns_dict={'neighbourhood': 'neighbourhood_freq'})
+    
+    # Encode the neighbourhood column using the frequency map from the training set for the validation set and testing set
+    X_val, _ = apply_frequency_encoding(X_val, fmap_column_dict={'neighbourhood': 'neighbourhood_freq'}, fmap=train_fmap)
+    X_test, _ = apply_frequency_encoding(X_test, fmap_column_dict={'neighbourhood': 'neighbourhood_freq'}, fmap=train_fmap)
+    # Drop the original neighbourhood and age column from the training, validation, and testing sets
+    for _df in [X_train, X_val, X_test]:
+        _df.drop(columns=['neighbourhood'], axis=1, inplace=True)
+        _df.drop(columns=['age'], axis=1, inplace=True)
+
+    # Scale the features
+    X_train_scaled, X_val_scaled, X_test_scaled = scale_features(X_train, X_val, X_test)
+    
+    # Undersample the training data
+    X_train_resampled, y_train_resampled = under_sample(X_train_scaled, y_train, random_state)
+    
+    # Run the model
+    model_runner = ModelRunner(model, X_train_resampled, X_test_scaled, y_train_resampled, y_test, X_val_scaled, y_val, pick_results)
+    return model_runner.invoke()
